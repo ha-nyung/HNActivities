6fb964f521d7a168d0773d654cd666a1c09dcbf0
Josh Chung <minorblend@gmail.com>
2013-01-24 23:15:07 +0900

    added unit testing

---
+++ demo/HNActivitiesTests/HNActivitiesTests.h
@@ -0,0 +1,13 @@
+//
+//  HNActivitiesTests.h
+//  HNActivitiesTests
+//
+//  Created by Josh Chung on 1/24/13.
+//  Copyright (c) 2013 minorblend. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface HNActivitiesTests : SenTestCase
+
+@end
---
+++ demo/HNActivitiesTests/HNActivitiesTests.m
@@ -0,0 +1,32 @@
+//
+//  HNActivitiesTests.m
+//  HNActivitiesTests
+//
+//  Created by Josh Chung on 1/24/13.
+//  Copyright (c) 2013 minorblend. All rights reserved.
+//
+
+#import "HNActivitiesTests.h"
+
+@implementation HNActivitiesTests
+
+- (void)setUp
+{
+  [super setUp];
+
+  // Set-up code here.
+}
+
+- (void)tearDown
+{
+  // Tear-down code here.
+
+  [super tearDown];
+}
+
+- (void)testExample
+{
+  STFail(@"Unit tests are not implemented yet in HNActivitiesTests");
+}
+
+@end
